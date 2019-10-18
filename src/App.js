938d9629468505a1def12e914b87185342a3f5ac
Taylor Case <tmcase16@gmail.com>
2019-10-18 16:41:02 -0400

    add links

--- src/App.js
+++ src/App.js
@@ -10,22 +10,33 @@ function App() {
       <section className="body">
         <p>Hi! I'm Taylor.</p>
 
-        <p>I'm a front-end engineer, currently working at <Link url="//myshyft.com" text="Shyft"/>.</p>
+        <p>
+          I'm a front-end engineer, currently working at{" "}
+          <Link url="//myshyft.com" text="Shyft" />.
+        </p>
 
         <p>I live in Seattle. </p>
 
-        <p>I love to read. </p>
+        <p>
+          I love to{" "}
+          <Link
+            url="https://www.goodreads.com/user/show/53231383-taylor-case"
+            text="read"
+          />
+          .{" "}
+        </p>
+
+        <p>I love to run. </p>
 
         <p>I love to travel. </p>
 
-        <p>I love to code.</p>
+        <p>
+          I love to <Link url="//github.com/tcase360" text="code" />.
+        </p>
 
         <p>I love to write.</p>
       </section>
-
-      <footer className="">
-
-      </footer>"
+      <footer className=""></footer>"
     </div>
   );
 }
