1193bf094e00235c9361c198926b2c6a24f2970f
Taylor Case <tmcase16@gmail.com>
2019-10-20 17:14:22 -0400

    create animation effect on load

--- src/_pages/Index/Index.jsx
+++ src/_pages/Index/Index.jsx
@@ -4,20 +4,57 @@ import Link from '../../_components/Link/Link';
 
 import './Index.css';
 
+function showOrHide(index, array) {
+  return array[index] ? 'show' : 'hide';
+}
+
 class IndexPage extends React.Component {
+  state = {
+    loadedCount: 0,
+    textsLoaded: {},
+    
+  };
+
+  componentDidMount() {
+    const interval = setInterval(this._renderNextPieceOfText, 50);
+
+    this.setState({
+      interval,
+    });
+  }
+
+  _renderNextPieceOfText = () => {
+    const { loadedCount, textsLoaded } = this.state;
+
+    textsLoaded[loadedCount] = 1;
+
+    this.setState({
+      loadedCount: loadedCount + 1,
+      textsLoaded
+    });
+
+    if (loadedCount === 8) {
+      this.setState({
+        interval: null,
+      });
+    }
+  };
+
   render() {
+    const { textsLoaded } = this.state;
+
     return (
       <StandardTemplate>
-        <p>Hi! I'm Taylor.</p>
+        <p className={showOrHide(1, textsLoaded)}>Hi! I'm Taylor.</p>
 
-        <p>
-          I'm a front-end engineer, currently working at{" "}
+        <p className={showOrHide(2, textsLoaded)}>
+          I'm a front-end engineer currently working at{" "}
           <Link url="//myshyft.com" text="Shyft" />.
         </p>
 
-        <p>I live in Seattle. </p>
+        <p className={showOrHide(3, textsLoaded)}>I live in Seattle. </p>
 
-        <p>
+        <p className={showOrHide(4, textsLoaded)}>
           I love to{" "}
           <Link
             url="https://www.goodreads.com/user/show/53231383-taylor-case"
@@ -26,15 +63,15 @@ class IndexPage extends React.Component {
           .{" "}
         </p>
 
-        <p>I love to run. </p>
+        <p className={showOrHide(5, textsLoaded)}>I love to run. </p>
 
-        <p>I love to travel. </p>
+        <p className={showOrHide(6, textsLoaded)}>I love to travel. </p>
 
-        <p>
+        <p className={showOrHide(7, textsLoaded)}>
           I love to <Link url="//github.com/tcase360" text="code" />.
         </p>
 
-        <p>I love to write.</p>
+        <p className={showOrHide(8, textsLoaded)}>I love to write.</p>
       </StandardTemplate>
     );
   }
