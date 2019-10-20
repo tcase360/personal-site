f2b09cb39f070f96fcab33158e62fe1298148297
Taylor Case <tmcase16@gmail.com>
2019-10-20 16:41:34 -0400

    add transition to header

--- src/_templates/Standard/Standard.jsx
+++ src/_templates/Standard/Standard.jsx
@@ -3,14 +3,35 @@ import React from 'react';
 import './Standard.css';
 
 class StandardTemplate extends React.Component {
+  state = {
+    headerLoaded: false,
+  };
+
+  componentDidMount() {
+    setTimeout(this._fadeInHeader, 100);
+  }
+
+  _fadeInHeader = () => {
+    this.setState({
+      headerLoaded: true
+    });
+  }
 
   render() {
+    const { headerLoaded } = this.state;
     const { children } = this.props;
+
+    const logoClassName = headerLoaded ? "show" : "hide";
 
     return (
       <div className="standard-template">
         <header className="header">
-          <a href="https://taylormcase.com">taylor case.</a>
+          <a 
+            href="https://taylormcase.com"
+            className={logoClassName}
+          >
+            Taylor Case
+          </a>
         </header>
 
         <div className="body">{children}</div>
